42fd414362c1a4faf0128bbe79a2728f5b86853e
ananthasubramaniam <144979769+Ananthasubramaniam@users.noreply.github.com>
2025-09-08 16:16:00 +0530

    dog face

--- dog.py
+++ dog.py
@@ -1,3 +1,14 @@
-n=5
-for i in range(0, 6):
-        print(" " * (n - i) + "*" * (2 * i - 1))
+def dog_face():
+    print("         / \\__      ")
+    print("       (    @\\___   ")
+    print("       /         O  ")
+    print("      /   (_____/   ")
+    print("     /_____/        ")
+    print("    /      \\        ")
+    print("   /  ^  ^  \\       ")
+    print("  (   o  o   )      ")
+    print("   |    ∆    |      ")
+    print("   |   ---   |      ")
+    print("    \\_______/       ")
+
+dog_face()
